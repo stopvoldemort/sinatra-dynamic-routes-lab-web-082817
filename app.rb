8a7d942b5acc2bdb481fa7144fdfc6905a4573c2
stopvoldemort <david.squires1@gmail.com>
2017-09-18 14:29:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    binding.pry
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    "#{params.map {|k,v| v}.join(" ")}."
   end
 
   get '/:operation/:number1/:number2' do
